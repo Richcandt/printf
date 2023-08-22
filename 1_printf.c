e55ae72e5d4af7e0ce8d01d24b1d7a0ac6912961
NiPaccy <nikuzepacifique22@gmail.com>
2023-08-22 13:21:21 -0700

    Task 1

--- 1_printf.c
+++ 1_printf.c
@@ -1,41 +1,71 @@
 #include "main.h"
 #include <stdio.h>
 
-int _printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
+int _printf(const char *format, ...)
+{
+	va_list args;
 
-    int count = 0;
-    
-    while (*format != '\0') {
-        if (*format == '%') {
-            format++; 
-            switch (*format) {
-                case 'c':
-                    putchar(va_arg(args, int));
-                    count++;
-                    break;
-                case 's': {
-                    char *str = va_arg(args, char *); 
-                    while (*str != '\0') {
-                        putchar(*str); 
-                        str++;
-                        count++;
-                    }
-                    break;
-                }
-                case 'd':
-                case 'i': {
-                    int num = va_arg(args, int);
-                    printf("%d", num); 
-                    count += num < 0 ? 2 : 1;  
-                    break;
-                }
-                case '%':
-                    putchar('%'); 
-                    count++;
-                    break;
-                default:
-                    
-                    break;
-            }
+I	va_start(args, format);
+
+	int count = 0;
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+				{
+					char *str = va_arg(args, char *);
+
+					while (*str != '\0')
+					{
+						putchar(*str);
+						str++;
+						count++;
+					}
+					break;
+				}
+				case 'd':
+				case 'i':
+				{
+					int num = va_arg(args, int);
+
+					printf("%d", num);
+					count += num < 0 ? 2 : 1;
+					break;
+				}
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				default:
+
+					break;
+			}
+
+		}	else
+		{
+			putchar(format);
+			count++;
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
+}
+
+int main(void)
+{
+	int num = -42;
+
+	_printf("Character: %c, String: %s, Integer: %d\n", 'A', "Hello", num);
+	return (0);
+}
