f5161c52f392f059ae7d945b6cc040f7f054397f
Richcandt <kwizerarichard27@gmail.com>
2023-08-22 10:26:01 -0700

    task 0

---
+++ _printf.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _printf - function that produces output according to a format.
+ * @format: character string
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int j = 0;
+	va_list args;
+
+	if (format == NULL)
+	return (-1);
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			j++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				j++;
+			}
+			else if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+
+				write(1, &c, 1);
+				j++;
+			}
