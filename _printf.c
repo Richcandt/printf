0a0f26907cc40fef828703360433dc44d023595a
NiPaccy <nikuzepacifique22@gmail.com>
2023-08-22 11:57:07 -0700

    Pairing

--- _printf.c
+++ _printf.c
@@ -38,3 +38,19 @@ int _printf(const char *format, ...)
 				write(1, &c, 1);
 				j++;
 			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char*);
+				int str_len = 0;
+
+				while (str[str_len] != '\0')
+				str_len++;
+				write(1, str, str_len);
+				j += str_len;
+			}
+		}
+		format++;
+	}
+	va_end(args);
+	return (j);
+}
